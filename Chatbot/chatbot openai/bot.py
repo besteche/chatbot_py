97bef13840c8eae88bd88ba510e5556e0d89702f
besteche <121049949+besteche@users.noreply.github.com>
2023-10-09 21:33:03 -0300

    Add files via upload

---
+++ Chatbot/chatbot openai/bot.py
@@ -0,0 +1,40 @@
+import discord
+import responses
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        if is_private:
+            await message.author.send(response)
+        else:
+            await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    Token = 'MTE2MTA2MzU1NDcyMDQ2OTA3NA.GzSOTW.R4x_3dH41BbWEdJZtQk0Zso5l5ZzgUVr3B8Fgo'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        print(f'{username} said "{user_message}" ({channel})')
+
+        if user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(Token)
+
